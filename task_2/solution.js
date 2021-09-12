8bb16ae141557ab7103853ac8858c300dcba79b6
Stepanova <stepanova.ma87al@gmail.com>
2021-09-12 16:48:10 +0300

    1d

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,17 +5,17 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    // создайте переменную shippingSum
+    let shippingSum// создайте переменную shippingSum
 
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
-
-    // если productsSum Больше или равна freeShippingMinSum,
-    // то shippingSum присвоить значение 0
-
-    // если productsSum больше 0 и меньше freeShippingMinSum,
-    // то shippingSum присвоить значение shippingPrice
-
+    if (productsSum === 0) {
+        console.log (shippingSum = 0)
+    }// если productsSum равно 0,// то shippingSum присвоить значение 0
+else if (productsSum >= freeShippingMinSum) {
+    console.log (shippingSum = 0)
+} // если productsSum Больше или равна freeShippingMinSum, // то shippingSum присвоить значение 0
+else if (productsSum > 0 &&productsSum < freeShippingMinSum) {// если productsSum больше 0 и меньше freeShippingMinSum,
+   console.log (shipping) // то shippingSum присвоить значение shippingPrice
+}
     // Конец решения задания №2.1.
 
     return shippingSum;
