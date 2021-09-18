b6a80a1b443009300af2c48f3f40618a65d889f9
Stepanova <stepanova.ma87al@gmail.com>
2021-09-18 15:26:51 +0300

    Task 2.1

--- task_2/solution.js
+++ task_2/solution.js
@@ -59,7 +59,7 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
    let freeShipping// создайте переменную freeShipping
     // запишите без использования if или любых других условий:
-   console.log  (freeShipping = (shippingSum = 0)? true : false) // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+   console.log  (shippingSum = 0? freeShipping =  true : freeShipping =  false) // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
 
     // Конец решения задачи №2.3.
 
