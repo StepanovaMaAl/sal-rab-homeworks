3712e4e02834e911d44d898985d1c9adc913b44d
Stepanova <stepanova.ma87al@gmail.com>
2021-09-19 10:02:17 +0300

    task 3/1

--- task_3/solution.js
+++ task_3/solution.js
@@ -14,14 +14,14 @@ function renderCartItem(item) {
     // количество × цена ₽ = стоимость ₽
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
-    // 1. Создайте переменную itemCountText, равную пустой строке
-    // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
-    // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
-    // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
-    // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
-    // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
-    // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
-    // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
+    let itemCountText = " "// 1. Создайте переменную itemCountText, равную пустой строке
+   let itemCountText = itemCountText + (product ['count']) // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
+    let itemCountTex = itemCountTex + ` × ` // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
+    let itemCountTex = itemCountTex + (product ['price']) // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
+    let itemCountTex = itemCountTex + ` ₽ = ` // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
+    let sum = (product ['count'] *product ['price'] )// 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
+    let itemCountText = itemCountText + sum// 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
+    let itemCountText = itemCountText + ` ₽` // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 
     // Конец решения задания №3.1.
 
