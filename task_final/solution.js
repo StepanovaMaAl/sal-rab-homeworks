c2a43c8c8e55b05c3714b27570102d8380925aac
Stepanova <stepanova.ma87al@gmail.com>
2021-10-29 20:16:44 +0300

    14

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,13 +34,18 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client:{}, order: {}, goods: [] };
-    let countOfGoods =  data.goods.length;
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push({"title": goods[i].title, "count": goods[i].count});
-    }
-    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
-    data.order.sum = sum;
-    data.client = `${name} ${phone}`;
-    let jsonData = JSON.stringify({data});
-    return jsonData;}
+    let data = {client: {} , order: {}, goods: []};
+
+   data.client = `${name} ${phone}`;
+   data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+   data.order.sum =  sum;
+   let countOfGoods = data.goods.length;
+   
+   for (let i = 0; i <= countOfGoods; i += 1) {
+       data.goods.push({"title": goods[i].title, "count": goods[i].count});
+   }
+   
+   let jsonData = JSON.stringify({data});
+
+   return jsonData;
+}
